4930eab59d7de5d026bb5bb08bc0dd036da7926e
Alice Khovrus <alice.khovrus@gmail.com>
2024-02-12 03:28:42 +0800

    small changes

--- extension/background.js
+++ extension/background.js
@@ -1,35 +1,16 @@
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    
     if (request.action == "executeScript") {
         const id = request.id;
-        fetch(`http://localhost:3000/getTest?id=${id}`)
-            .then(response => response.text())
-            .then(scriptContent => {
-                chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-                    const activeTab = tabs[0];
-                    chrome.scripting.executeScript({
-                        target: { tabId: activeTab.id },
-                        function: function(scriptContent) {
-                            const scriptFunction = new Function(scriptContent);
-                            scriptFunction();
-                        },
-                        args: [scriptContent],
-                    });
-                });
-            })
-            .catch(error => console.error(error));
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+            const activeTab = tabs[0];
+            chrome.scripting.executeScript({
+                target: { tabId: activeTab.id },
+                files: [`tests/html/lab${id}.js`]
+            });
+        });
     }
-
-    // if (request.action == "executeScript") {
-    //     const id = request.id;
-    //     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    //         const activeTab = tabs[0];
-    //         chrome.scripting.executeScript({
-    //             target: { tabId: activeTab.id },
-    //             files: [`tests/html/lab${id}.js`]
-    //         });
-    //     });
-    // }
 
     if (request.action == "sendResult") {
         chrome.runtime.sendMessage({ action: "showResult", result: request.result });
